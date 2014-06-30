372e485b76b2387592bb195b00c876707bc5b220
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-01 01:40:03 +0800

    Read and write dhcpd.conf

--- dhcp/Main.c
+++ dhcp/Main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 #include "File.h"
 
 int Activate(int count, char * values [])
@@ -11,5 +13,33 @@ int Activate(int count, char * values [])
 	{
 		printf("Argument %d: %s\n",i+1,values[i]);
 	}
+
+	unsigned long fileSize=GetFileSize("/etc/dhcp/dhcpd.conf");
+	printf("File Size: %lu byte(s)\n",fileSize);
+	char * fileContent=malloc((unsigned int)fileSize+1000);
+	memset(fileContent,0,fileSize);
+
+	ReadAllText("/etc/dhcp/dhcpd.conf",fileContent);
+	printf("File Content:\n");
+	for(i=0;i<fileSize;i++)
+	{
+		printf("%c",fileContent[i]);
+	}
+
+	strcat(fileContent,"This is a test.\n");
+
+	WriteAllText("/etc/dhcp/dhcpd.conf",fileContent);
+
+	fileSize=GetFileSize("/etc/dhcp/dhcpd.conf");
+	printf("New File Size: %lu byte(s)\n",fileSize);
+	ReadAllText("/etc/dhcp/dhcpd.conf",fileContent);
+	printf("New File Content:\n");
+	for(i=0;i<fileSize;i++)
+	{
+		printf("%c",fileContent[i]);
+	}
+
+	free(fileContent);
+
 	return 0;
 }
