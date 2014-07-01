9ca2038d0f3b38f7a9e7d13908dc0ff185830a3e
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-02 00:54:54 +0800

    Installer updated.

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 FILE_NAME=/tmp/netsh.tar.gz
-
+echo "Net shell installer"
 sed -n -e '1,/^exit 0$/!p' $0 > $FILE_NAME 2>/dev/null
 mkdir -p /usr/local/netsh
 tar -zxf $FILE_NAME -C /usr/local/netsh
 rm -rf $FILE_NAME
+echo "Installed to /usr/local/netsh. Enter \"netsh\" for more information."
+echo "Document directory: /usr/local/netsh/doc"
 chmod +x /usr/local/netsh/netsh
 ln -sf /usr/local/netsh/netsh /bin/netsh
+echo "Done."
 exit 0
