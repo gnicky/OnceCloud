b7b3536203bda4ae26625fec77678fd798c1ad69
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-01 17:44:12 +0800

    Add command to bind new IP and MAC.

--- dhcp/Main.c
+++ dhcp/Main.c
@@ -3,43 +3,97 @@
 #include <memory.h>
 #include "File.h"
 
-int Activate(int count, char * values [])
+const char * DhcpdConfigurationFileName="/etc/dhcp/dhcpd.conf";
+
+void GenerateConfiguration(char * buffer, const char * ipAddress, const char * hardwareAddress)
 {
-	printf("DHCP Plugin\n");
-	printf("Count: %d\n",count);
-	printf("Values:\n");
+	buffer[0]='\0';
+
+	strcat(buffer,"\thost ");
+	strcat(buffer,ipAddress);
+	strcat(buffer," {\n");
+
+	strcat(buffer,"\t\thardware ethernet ");
+	strcat(buffer,hardwareAddress);
+	strcat(buffer,";\n");
+
+	strcat(buffer,"\t\tfixed-address ");
+	strcat(buffer,ipAddress);
+	strcat(buffer,";\n");
+
+	strcat(buffer,"\t}\n");
+}
+
+void PrintUsage()
+{
+	printf("Usage:\n");
+	printf("Bind IP and MAC:\n\tnetsh dhcp bind [IP] [MAC]\n");
+	printf("Unbind IP:\n\tnetsh dhcp unbind [IP]\n");
+}
+
+int Bind(const char * ipAddress, const char * hardwareAddress)
+{
+	printf("Bind %s to %s.\n",ipAddress,hardwareAddress);
+	if(!IsFileExist(DhcpdConfigurationFileName))
+	{
+		printf("[Error] File is not exist: %s\n",DhcpdConfigurationFileName);
+		return 1;
+	}
+	int fileSize=(int)GetFileSize(DhcpdConfigurationFileName);
+	char * fileContent=malloc(fileSize);
+	ReadAllText(DhcpdConfigurationFileName,fileContent);
+
+	char * position=strstr(fileContent,"\n}");
+	if(position==NULL)
+	{
+		printf("[Error] Malformed configuration file.\n");
+		return 1;
+	}
+	char * newFileContent=malloc(fileSize+1000);
+	memset(newFileContent,0,fileSize+1000);
+	*position='\0';
+	strcat(newFileContent,fileContent);
+	strcat(newFileContent,"\n");
+	char buffer[1000];
+	GenerateConfiguration(buffer,ipAddress,hardwareAddress);
+	strcat(newFileContent,buffer);
+	strcat(newFileContent,position+1);
+	int length=strlen(newFileContent);
 	int i=0;
-	for(i=0;i<count;i++)
+	for(i=0;i<length;i++)
 	{
-		printf("Argument %d: %s\n",i+1,values[i]);
+		printf("%c",newFileContent[i]);
 	}
-
-	unsigned long fileSize=GetFileSize("/etc/dhcp/dhcpd.conf");
-	printf("File Size: %lu byte(s)\n",fileSize);
-	char * fileContent=malloc((unsigned int)fileSize+1000);
-	memset(fileContent,0,fileSize);
-
-	ReadAllText("/etc/dhcp/dhcpd.conf",fileContent);
-	printf("File Content:\n");
-	for(i=0;i<fileSize;i++)
-	{
-		printf("%c",fileContent[i]);
-	}
-
-	strcat(fileContent,"This is a test.\n");
-
-	WriteAllText("/etc/dhcp/dhcpd.conf",fileContent);
-
-	fileSize=GetFileSize("/etc/dhcp/dhcpd.conf");
-	printf("New File Size: %lu byte(s)\n",fileSize);
-	ReadAllText("/etc/dhcp/dhcpd.conf",fileContent);
-	printf("New File Content:\n");
-	for(i=0;i<fileSize;i++)
-	{
-		printf("%c",fileContent[i]);
-	}
+	
+	free(newFileContent);
 
 	free(fileContent);
-
 	return 0;
+}
+
+int Unbind(const char * ipAddress)
+{
+	printf("Unbind %s.\n",ipAddress);
+	return 0;
+}
+
+int Activate(int count, char * values [])
+{
+	if(count==2)
+	{
+		if(strcmp(values[0],"unbind")==0)
+		{
+			return Unbind(values[1]);
+		}
+	}
+	if(count==3)
+	{
+		if(strcmp(values[0],"bind")==0)
+		{
+			return Bind(values[1],values[2]);
+		}
+	}
+
+	PrintUsage();
+	return 1;
 }
