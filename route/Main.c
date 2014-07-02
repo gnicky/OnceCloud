1b9e91e1f93b74738297e81395c6f678f075b602
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-02 23:31:16 +0800

    Command "netsh route remove" added.

--- route/Main.c
+++ route/Main.c
@@ -28,7 +28,6 @@ void GeneratePreRoutingRule(char * buffer, const char * internal, const char * e
 	strcat(buffer,external);
 	strcat(buffer,"/32 -j DNAT --to-destination ");
 	strcat(buffer,internal);
-	strcat(buffer,"\n");
 }
 
 void GeneratePostRoutingRule(char * buffer, const char * internal, const char * external)
@@ -38,7 +37,6 @@ void GeneratePostRoutingRule(char * buffer, const char * internal, const char * 
 	strcat(buffer,internal);
 	strcat(buffer,"/32 -j SNAT --to-source ");
 	strcat(buffer,external);
-	strcat(buffer,"\n");
 }
 
 int AddNat(const char * internal, const char * external)
@@ -100,18 +98,13 @@ int AddNat(const char * internal, const char * external)
 		GeneratePreRoutingRule(preRoutingRule,internal,external);
 		GeneratePostRoutingRule(postRoutingRule,internal,external);
 		strcat(newConfiguration,preRoutingRule);
+		strcat(newConfiguration,"\n");
 		strcat(newConfiguration,postRoutingRule);
+		strcat(newConfiguration,"\n");
 	}
 	
 	strcat(newConfiguration,ruleEnd);
 
-	int length=strlen(newConfiguration);
-	int i=0;
-	for(i=0;i<length;i++)
-	{
-		printf("%c",newConfiguration[i]);
-	}	
-	
 	SaveConfiguration(newConfiguration);
 	
 	free(newConfiguration);
@@ -121,6 +114,70 @@ int AddNat(const char * internal, const char * external)
 
 int RemoveNat(const char * internal, const char * external)
 {
+	char savedChar;
+	char * originalConfiguration=malloc(1048576);
+	char * newConfiguration=malloc(1048576);
+	newConfiguration[0]='\0';
+
+	LoadConfiguration(originalConfiguration);
+	char * natStart=strstr(originalConfiguration,"*nat");
+
+	if(natStart==NULL)
+	{
+		free(newConfiguration);
+		free(originalConfiguration);
+		return 0;
+	}
+
+	savedChar=*natStart;
+	*natStart='\0';
+	strcat(newConfiguration,originalConfiguration);
+	*natStart=savedChar;
+
+	char * ruleStart=strstr(natStart,":OUTPUT");
+	ruleStart=strstr(ruleStart,"\n");
+	savedChar=*ruleStart;
+	*ruleStart='\0';
+	strcat(newConfiguration,natStart);
+	*ruleStart=savedChar;
+
+	// From the start of the rule
+	
+	char preRoutingRule[1000]={0};
+	char postRoutingRule[1000]={0};
+	GeneratePreRoutingRule(preRoutingRule,internal,external);
+	GeneratePostRoutingRule(postRoutingRule,internal,external);
+
+	strcat(newConfiguration,"\n");
+	char * position=ruleStart+1;
+
+	while(strstr(position,"COMMIT")!=NULL && strstr(position,"COMMIT")!=position)
+	{
+		if(strstr(position,preRoutingRule)==position
+			|| strstr(position,postRoutingRule)==position)
+		{
+			position=strstr(position,"\n");
+			position=position+strlen("\n");
+		}
+		else
+		{
+			char * lineEnd=strstr(position,"\n");
+			*lineEnd='\0';
+			strcat(newConfiguration,position);
+			strcat(newConfiguration,"\n");
+			*lineEnd='\n';
+			position=lineEnd;
+			position=position+strlen("\n");
+		}
+	}
+
+	char * ruleEnd=position;
+	strcat(newConfiguration,ruleEnd);
+
+	SaveConfiguration(newConfiguration);
+
+	free(newConfiguration);
+	free(originalConfiguration);
 	return 0;
 }
 
