fd7ef97219c7ed7e30acc7ad98ee98695278354e
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-03 15:58:35 +0800

    Remove target directory before create in install script.

--- installer/installer.sh
+++ installer/installer.sh
@@ -2,6 +2,7 @@
 FILE_NAME=/tmp/netsh.tar.gz
 echo "Net shell installer"
 sed -n -e '1,/^exit 0$/!p' $0 > $FILE_NAME 2>/dev/null
+rm -rf /usr/local/netsh
 mkdir -p /usr/local/netsh
 tar -zxf $FILE_NAME -C /usr/local/netsh
 rm -rf $FILE_NAME
