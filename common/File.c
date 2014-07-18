a43293e44c193b1d310d191c5a1c849eea8a8898
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-18 16:32:54 +0800

    Check arguments and provide friendly error message in File operations

--- common/File.c
+++ common/File.c
@@ -12,6 +12,7 @@ int IsFileExist(const char * path)
 {
 	if(path==NULL)
 	{
+		WriteLog(LOG_ERR,"IsFileExist: Argument null: path");
 		return FALSE;
 	}
 
@@ -19,6 +20,7 @@ int IsFileExist(const char * path)
 	{
 		return TRUE;
 	}
+
 	return FALSE;
 }
 
@@ -26,15 +28,16 @@ int IsDirectoryExist(const char * path)
 {
 	if(path==NULL)
 	{
+		WriteLog(LOG_ERR,"IsDirectoryExist: Argument null: path");
 		return FALSE;
 	}
 
 	DIR * pointer=opendir(path);
 	if(pointer==NULL)
 	{
-		WriteLog(LOG_ERR,strerror(errno));
 		return FALSE;
 	}
+
 	closedir(pointer);
 	return TRUE;
 }
@@ -43,13 +46,14 @@ int GetFileSize(const char * fileName)
 {
 	if(fileName==NULL)
 	{
+		WriteLog(LOG_ERR,"GetFileSize: Argument null: fileName");
 		return -1;
 	}
 
 	struct stat fileStatus;
 	if(stat(fileName,&fileStatus)<0)
 	{
-		WriteLog(LOG_ERR,strerror(errno));
+		WriteLog(LOG_ERR,"GetFileSize: Cannot get status of %s (%s)",fileName,strerror(errno));
 		return -1;
 	}
 	return (int)fileStatus.st_size;
@@ -57,10 +61,16 @@ int GetFileSize(const char * fileName)
 
 int CreateDirectory(const char * path)
 {
+	if(path==NULL)
+	{
+		WriteLog(LOG_ERR,"CreateDirectory: Argument null: path");
+		return FALSE;
+	}
+
 	int status=mkdir(path,S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH);
 	if(status!=0)
 	{
-		WriteLog(LOG_ERR,strerror(errno));
+		WriteLog(LOG_ERR,"CreateDirectory: Cannot create directory %s (%s)",path,strerror(errno));
 		return FALSE;
 	}
 	return TRUE;
@@ -68,15 +78,34 @@ int CreateDirectory(const char * path)
 
 int CreateDirectoryIfNotExist(const char * path)
 {
+	if(path==NULL)
+	{
+		WriteLog(LOG_ERR,"CreateDirectoryIfNotExist: Argument null: path");	
+		return FALSE;
+	}
+
 	if(IsDirectoryExist(path))
 	{
 		return TRUE;
 	}
+
 	return CreateDirectory(path);
 }
 
 int ReadFile(const char * fileName, char * fileContent)
 {
+	if(fileName==NULL)
+	{
+		WriteLog(LOG_ERR,"ReadFile: Argument null: fileName");
+		return FALSE;
+	}
+
+	if(fileContent==NULL)
+	{
+		WriteLog(LOG_ERR,"ReadFile: Argument null: fileContent");
+		return FALSE;
+	}
+
 	int expectedReadCount;
 	int actualReadCount;
 	FILE * file=NULL;
@@ -84,7 +113,7 @@ int ReadFile(const char * fileName, char * fileContent)
 	file=fopen(fileName,"r");
 	if(file==NULL)
 	{
-		WriteLog(LOG_ERR,strerror(errno));	
+		WriteLog(LOG_ERR,"ReadFile: Cannot open file %s (%s)",fileName,strerror(errno));	
 		return FALSE;
 	}
 
@@ -92,6 +121,7 @@ int ReadFile(const char * fileName, char * fileContent)
 	actualReadCount=fread(fileContent,sizeof(char),expectedReadCount,file);
 	if(actualReadCount!=expectedReadCount)
 	{
+		WriteLog(LOG_ERR,"ReadFile: Expected and actual read bytes don't match");	
 		return FALSE;
 	}
 
@@ -101,6 +131,18 @@ int ReadFile(const char * fileName, char * fileContent)
 
 int WriteFile(const char * fileName, const char * fileContent)
 {
+	if(fileName==NULL)
+	{
+		WriteLog(LOG_ERR,"WriteFile: Argument null: fileName");
+		return FALSE;
+	}
+
+	if(fileContent==NULL)
+	{
+		WriteLog(LOG_ERR,"WriteFile: Argument null: fileContent");
+		return FALSE;
+	}
+
 	FILE * file=NULL;
 	int expectedWriteCount;
 	int actualWriteCount;
@@ -108,7 +150,7 @@ int WriteFile(const char * fileName, const char * fileContent)
 	file=fopen(fileName,"w");
 	if(file==NULL)
 	{
-		WriteLog(LOG_ERR,strerror(errno));
+		WriteLog(LOG_ERR,"WriteFile: Cannot open file %s (%s)",fileName,strerror(errno));
 		return FALSE;
 	}
 
@@ -116,6 +158,7 @@ int WriteFile(const char * fileName, const char * fileContent)
 	actualWriteCount=fwrite(fileContent,sizeof(char),expectedWriteCount,file);
 	if(actualWriteCount!=expectedWriteCount)
 	{
+		WriteLog(LOG_ERR,"WriteFile: Expected and actual write bytes don't match");	
 		return FALSE;
 	}
 
@@ -126,6 +169,30 @@ int WriteFile(const char * fileName, const char * fileContent)
 
 int ListFiles(const char * path, const char * suffix, int * count, char ** buffer)
 {
+	if(path==NULL)
+	{
+		WriteLog(LOG_ERR,"ListFiles: Argument null: path");
+		return FALSE;
+	}
+
+	if(suffix==NULL)
+	{
+		WriteLog(LOG_ERR,"ListFiles: Argument null: suffix");
+		return FALSE;
+	}
+
+	if(count==NULL)
+	{
+		WriteLog(LOG_ERR,"ListFiles: Argument null: count");
+		return FALSE;
+	}
+
+	if(buffer==NULL)
+	{
+		WriteLog(LOG_ERR,"ListFiles: Argument null: buffer");
+		return FALSE;
+	}
+
 	int i=0;
 	DIR * directory=NULL;
 	struct dirent * entry=NULL;
@@ -133,7 +200,7 @@ int ListFiles(const char * path, const char * suffix, int * count, char ** buffe
 	directory=opendir(path);
 	if(directory==NULL)
 	{
-		WriteLog(LOG_ERR,strerror(errno));
+		WriteLog(LOG_ERR,"ListFiles: Cannot open directory %s (%s)",path,strerror(errno));
 		return FALSE;
 	}
 
