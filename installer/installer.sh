54795dc76c61ed68cca8190c392c2e3ced5671b0
Zhen Tang <silenceofdaybreak@gmail.com>
2014-07-25 22:33:10 +0800

    Installer updated, only main program and plugins will be updated.

--- installer/installer.sh
+++ installer/installer.sh
@@ -3,8 +3,12 @@ FILE_NAME=/tmp/netd.tar.gz
 
 sed -n -e '1,/^exit 0$/!p' $0 > $FILE_NAME 2>/dev/null
 
-rm -rf /usr/local/netd
+rm -rf /usr/local/netd/plugins/*
+rm -rf /usr/local/netd/netd
+
 mkdir -p /usr/local/netd
+mkdir -p /usr/local/netd/plugins
+mkdir -p /usr/local/netd/log
 
 tar -zxf $FILE_NAME -C /usr/local/netd
 rm -rf $FILE_NAME
