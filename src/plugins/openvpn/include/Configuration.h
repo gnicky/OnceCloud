7de1a58a7be2c38d12a2b552fc2bd3a8d038ca30
Zhen Tang <silenceofdaybreak@gmail.com>
2014-08-15 10:15:11 +0800

    OpenVPN Configuration definition updated.

--- src/plugins/openvpn/include/Configuration.h
+++ src/plugins/openvpn/include/Configuration.h
@@ -3,10 +3,13 @@
 
 struct Configuration
 {
-	char CACertificate[4096];
-	char ServerCertificate[4096];
-	char ServerPrivateKey[4096];
-	char DiffieHellmanParameter[4096];
+	char Protocol[10];
+	int Port;
+	char CACertificate[32768];
+	char ServerCertificate[32768];
+	char ServerPrivateKey[32768];
+	char DiffieHellmanParameter[32768];
+	char TLSAuthKey[32768];
 	char NetworkAddress[50];
 	char Netmask[50];
 };
