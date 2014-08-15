55190028f36ece0f188ceda66acf9ccb961b69e2
Zhen Tang <silenceofdaybreak@gmail.com>
2014-08-15 17:22:25 +0800

    RESTful API for OpenVPN Plugin added.

--- src/plugins/openvpn/Api.c
+++ src/plugins/openvpn/Api.c
@@ -5,6 +5,7 @@
 #include "PluginInterface.h"
 #include "Core.h"
 #include "File.h"
+#include "Frozen.h"
 
 const char * PluginName="OpenVPN";
 const char * PluginVersion="1.0.0.0";
@@ -56,18 +57,102 @@ int HandlePostRequest(struct HttpRequest * request, struct HttpResponse * respon
 	return TRUE;
 }
 
+int ReadTextValue(struct json_token * object, const char * key, char * buffer)
+{
+	const struct json_token * token;
+	token=find_json_token(object,key);
+	if(token==NULL)
+	{
+		return 1;
+	}
+	memcpy(buffer,token->ptr,token->len);
+	buffer[token->len]='\0';
+	return 0;
+}
+
+int ParseConfiguration(const char * json, struct Configuration * configuration)
+{
+	struct json_token * object;
+	
+	object=parse_json2(json,strlen(json));
+	if(object==NULL)
+	{
+		return FALSE;
+	}
+
+	int status=0;
+
+	status=ReadTextValue(object,"protocol",configuration->Protocol);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	char temp[100];
+	status=ReadTextValue(object,"port",temp);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+	sscanf(temp,"%d",&configuration->Port);
+
+
+	status=ReadTextValue(object,"caCertificate",configuration->CACertificate);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"serverCertificate",configuration->ServerCertificate);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"serverPrivateKey",configuration->ServerPrivateKey);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"diffieHellmanParameter",configuration->DiffieHellmanParameter);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"tlsAuthKey",configuration->TLSAuthKey);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"networkAddress",configuration->NetworkAddress);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	status=ReadTextValue(object,"netmask",configuration->Netmask);
+	if(status!=0)
+	{
+		return FALSE;
+	}
+
+	free(object);
+	return TRUE;
+}
+
 int HandlePutRequest(struct HttpRequest * request, struct HttpResponse * response)
 {
 	struct Configuration configuration;
-	strcpy(configuration.Protocol,"tcp");
-	configuration.Port=10086;
-	ReadFile("/root/keys/ca.crt",configuration.CACertificate);
-	ReadFile("/root/keys/server.crt",configuration.ServerCertificate);
-	ReadFile("/root/keys/server.key",configuration.ServerPrivateKey);
-	ReadFile("/root/keys/dh2048.pem",configuration.DiffieHellmanParameter);
-	ReadFile("/root/keys/ta.key",configuration.TLSAuthKey);
-	strcpy(configuration.NetworkAddress,"172.16.5.0");
-	strcpy(configuration.Netmask,"255.255.255.0");
+	int ret=ParseConfiguration(request->Content,&configuration);
+	if(ret!=TRUE)
+	{
+		response->StatusCode=400;
+		response->SetContent(response,"");
+		return TRUE;
+	}
 
 	Configure(&configuration);
 
