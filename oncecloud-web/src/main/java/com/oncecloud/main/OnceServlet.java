9046a06edeeda7fa259d63b3212d0d3894881c96
Zhen Tang <silenceofdaybreak@gmail.com>
2014-08-29 10:07:30 +0800

    QuotaManager added.

--- oncecloud-web/src/main/java/com/oncecloud/main/OnceServlet.java
+++ oncecloud-web/src/main/java/com/oncecloud/main/OnceServlet.java
@@ -3,18 +3,32 @@ package com.oncecloud.main;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.oncecloud.dao.FeeDAO;
 import com.oncecloud.thread.UpdateFeeThread;
 
 /**
  * @author hehai
  * @version 2014/06/26
  */
+@Component
 public class OnceServlet extends HttpServlet {
 	private static final long serialVersionUID = 8748334845182951985L;
+	private FeeDAO feeDAO;
+
+	private FeeDAO getFeeDAO() {
+		return feeDAO;
+	}
+
+	@Autowired
+	private void setFeeDAO(FeeDAO feeDAO) {
+		this.feeDAO = feeDAO;
+	}
 
 	public void init() throws ServletException {
-		// TODO: Inject FeeDAO
-		UpdateFeeThread uft = new UpdateFeeThread(null);
+		UpdateFeeThread uft = new UpdateFeeThread(this.getFeeDAO());
 		uft.start();
 		// RecvVMSyncThread rvst = new RecvVMSyncThread();
 		// rvst.start();
