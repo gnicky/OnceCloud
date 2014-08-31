e3329fc9a4f6d57ad07fcbc36abe3fd2ced9d6eb
Zhen Tang <silenceofdaybreak@gmail.com>
2014-08-31 21:36:00 +0800

    ChageDAO updated.

--- oncecloud-web/src/main/java/com/oncecloud/dao/ChargeDAO.java
+++ oncecloud-web/src/main/java/com/oncecloud/dao/ChargeDAO.java
@@ -3,9 +3,12 @@ package com.oncecloud.dao;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -28,121 +31,108 @@ public class ChargeDAO {
 	public ChargeRecord createChargeRecord(String recordId, Double recordBill,
 			Integer recordType, Date recordDate, Integer recordUID,
 			Integer state) {
-		ChargeRecord cr = null;
 		Session session = null;
-		Transaction tx = null;
 		try {
-			cr = new ChargeRecord(recordId, recordBill, recordType, recordDate,
-					recordUID);
+			session = this.getSessionHelper().getMainSession();
+			session.beginTransaction();
+			ChargeRecord cr = new ChargeRecord(recordId, recordBill,
+					recordType, recordDate, recordUID);
 			cr.setRecordState(state);
-			session = this.getSessionHelper().openMainSession();
-			tx = session.beginTransaction();
 			session.save(cr);
-			tx.commit();
+			session.getTransaction().commit();
+			return cr;
 		} catch (Exception e) {
 			e.printStackTrace();
-			if (tx != null) {
-				tx.rollback();
+			if (session != null) {
+				session.getTransaction().rollback();
 			}
 			return null;
-		} finally {
-			if (session != null && session.isOpen()) {
-				session.close();
-			}
 		}
-		return cr;
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<ChargeRecord> getOnePageChargeRecord(int page, int limit,
 			int userid) {
-		List<ChargeRecord> crList = null;
 		Session session = null;
 		try {
-			session = this.getSessionHelper().openMainSession();
+			session = this.getSessionHelper().getMainSession();
+			session.beginTransaction();
 			int startPos = (page - 1) * limit;
-			String queryString = "from ChargeRecord where recordUID=:userid and recordState > 0 order by recordDate desc";
-			Query query = session.createQuery(queryString);
-			query.setInteger("userid", userid);
-			query.setFirstResult(startPos);
-			query.setMaxResults(limit);
-			crList = query.list();
+			Criteria criteria = session.createCriteria(ChargeRecord.class)
+					.add(Restrictions.eq("recordUID", userid))
+					.add(Restrictions.gt("recordState", 0))
+					.addOrder(Order.desc("recordDate"));
+			criteria.setFirstResult(startPos);
+			criteria.setMaxResults(limit);
+			List<ChargeRecord> list = criteria.list();
+			session.getTransaction().commit();
+			return list;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (session != null && session.isOpen()) {
-				session.close();
+			if (session != null) {
+				session.getTransaction().rollback();
 			}
+			return null;
 		}
-		return crList;
 	}
 
 	public int countChargeRecord(int userid) {
-		int total = 0;
 		Session session = null;
 		try {
-			session = this.getSessionHelper().openMainSession();
-			String queryString = "select count(*) from ChargeRecord where recordUID=:userid and recordState > 0 ";
-			Query query = session.createQuery(queryString);
-			query.setInteger("userid", userid);
-			total = ((Number) query.iterate().next()).intValue();
+			session = this.getSessionHelper().getMainSession();
+			session.beginTransaction();
+			Criteria criteria = session.createCriteria(ChargeRecord.class)
+					.add(Restrictions.eq("recordUID", userid))
+					.add(Restrictions.gt("recordState", 0))
+					.setProjection(Projections.rowCount());
+			int total = ((Number) criteria.uniqueResult()).intValue();
+			session.getTransaction().commit();
+			return total;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (session != null && session.isOpen()) {
-				session.close();
+			if (session != null) {
+				session.getTransaction().rollback();
 			}
+			return 0;
 		}
-		return total;
 	}
 
 	public boolean updateChargeRecord(String recordId, Integer recordstate) {
-		boolean result = false;
 		Session session = null;
-		Transaction tx = null;
 		try {
-			session = this.getSessionHelper().openMainSession();
-			tx = session.beginTransaction();
+			session = this.getSessionHelper().getMainSession();
+			session.beginTransaction();
 			String queryString = "update ChargeRecord set recordState="
 					+ recordstate + " where recordId ='" + recordId + "'";
 			Query query = session.createQuery(queryString);
 			query.executeUpdate();
-			tx.commit();
-			result = true;
+			session.getTransaction().commit();
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
-			if (tx != null) {
-				tx.rollback();
+			if (session != null) {
+				session.getTransaction().rollback();
 			}
-		} finally {
-			if (session != null && session.isOpen()) {
-				session.close();
-			}
+			return false;
 		}
-		return result;
 	}
 
-	@SuppressWarnings("unchecked")
 	public ChargeRecord getChargeRecord(String recordId) {
-		List<ChargeRecord> crList = null;
 		Session session = null;
 		try {
-			session = this.getSessionHelper().openMainSession();
-			String queryString = "from ChargeRecord where recordId='"
-					+ recordId + "'";
-			Query query = session.createQuery(queryString);
-			crList = query.list();
-			if (crList.size() > 0) {
-				return crList.get(0);
-			}
+			session = this.getSessionHelper().getMainSession();
+			session.beginTransaction();
+			Criteria criteria = session.createCriteria(ChargeRecord.class).add(
+					Restrictions.eq("recordId", recordId));
+			ChargeRecord chargeRecord = (ChargeRecord) criteria.uniqueResult();
+			session.getTransaction().commit();
+			return chargeRecord;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return null;
-		} finally {
-			if (session != null && session.isOpen()) {
-				session.close();
+			if (session != null) {
+				session.getTransaction().rollback();
 			}
+			return null;
 		}
-		return null;
 	}
 }
