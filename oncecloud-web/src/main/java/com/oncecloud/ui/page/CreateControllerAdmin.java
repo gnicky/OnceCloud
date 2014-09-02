dd5af15e1d91decb9751d54e812b6a40d7521555
lokikiller <haotingyi13@otcaix.iscas.ac.cn>
2014-09-02 14:17:54 +0800

    hty

--- oncecloud-web/src/main/java/com/oncecloud/ui/page/CreateControllerAdmin.java
+++ oncecloud-web/src/main/java/com/oncecloud/ui/page/CreateControllerAdmin.java
@@ -1,8 +1,18 @@
 package com.oncecloud.ui.page;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CreateControllerAdmin {
 
+	@RequestMapping(value = "/user/create", method = { RequestMethod.GET })
+	public ModelAndView createVM(HttpServletRequest request) {
+		return new ModelAndView("user/create/createinstance");
+	}
+	
 }
