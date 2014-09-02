57273ebeff706207125d7b47a844e8562018c8da
hehai <hehai@192.168.2.4>
2014-09-02 10:12:07 +0800

    Web Socket Fix

--- oncecloud-web/src/main/webapp/js/message.js
+++ oncecloud-web/src/main/webapp/js/message.js
@@ -21,7 +21,7 @@ function connect() {
             var obj = JSON.parse(event.data);
             console.log('message type: ' + obj.messageType);
             console.log('content: ' + obj.content);
-            eval("(" + obj.messageType + "(" + obj.content + ")" + ")");
+            eval("(" + obj.messageType + "(" + obj + ")" + ")");
         }
 
     } else {
@@ -40,30 +40,57 @@ function send(message) {
     }));
 }
 
-function ws_sticky(content) {
-
+function ws_sticky(obj) {
+    $.sticky(obj.content);
 }
 
-function ws_delete_row(rowId) {
-
+function ws_delete_row(obj) {
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    $(thistr).remove();
 }
 
 function ws_edit_row_console(rowId, option) {
-
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    if (thistr.size() == 1) {
+        if (obj.option == "add") {
+            var thistd = thistr.find('[name="console"]');
+            if (thistd.find('.console').size() == 0) {
+                thistd.append('<a class="console" data-uuid='
+                    + obj.rowId + '><img src="../img/user/console.png"></a>');
+            }
+        } else {
+            thistr.find('.console').remove();
+        }
+    }
 }
 
 function ws_edit_row_ip(rowId, network, ip) {
-
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    if (thistr.size() == 1) {
+        thistr.find('[name="sip"]').html('<a>(' + obj.network + ')&nbsp;/&nbsp;' + obj.ip + '</a>');
+    }
 }
 
 function ws_edit_row_for_bind_volume(rowId, vmId, vmName) {
-
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    if (thistr.size() == 1) {
+        var vmtd = thistr.find('[vmuuid]').attr('vmuuid', obj.vmId).html('<a><span class="glyphicon glyphicon-cloud"></span>&nbsp;&nbsp;' + obj.vmName + '</a>');
+    }
 }
 
 function ws_edit_row_for_unbind_volume(rowId) {
-
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    if (thistr.size() == 1) {
+        var vmtd = thistr.find('[vmuuid]').attr('vmuuid', "").text("");
+    }
 }
 
 function ws_edit_row_status(rowId, icon, word) {
-
+    var thistr = $('#tablebody').find('[rowid="' + obj.rowId + '"]');
+    if (thistr.size() == 1) {
+        thistr.find('[name="stateicon"]').removeClass();
+        thistr.find('[name="stateicon"]').addClass("icon-status");
+        thistr.find('[name="stateicon"]').addClass("icon-" + obj.icon);
+        thistr.find('[name="stateword"]').text(obj.word);
+    }
 }
