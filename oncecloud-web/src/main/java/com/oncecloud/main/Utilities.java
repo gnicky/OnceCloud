180a56d933afa769d49d413e43ea05b20cec3d32
lokikiller <haotingyi13@otcaix.iscas.ac.cn>
2014-09-10 14:08:12 +0800

    hty

--- oncecloud-web/src/main/java/com/oncecloud/main/Utilities.java
+++ oncecloud-web/src/main/java/com/oncecloud/main/Utilities.java
@@ -7,6 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.JSONObject;
 
@@ -196,4 +198,11 @@ public class Utilities {
 		}
 		return result;
 	}
+	
+	public static boolean hasForbiddenChar(String str) {
+		Pattern pattern = Pattern.compile("[`~!$%^&#*()+|\\\\\\]\\[\\]\\{\\}:;'\\,<>?]+");
+		Matcher m = pattern.matcher(str);
+		return m.find();
+	}
+
 }
