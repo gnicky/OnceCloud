a36d1cc33c598e2500618868b833681cd616f93f
lokikiller <316064769@qq.com>
2014-11-19 08:54:39 +0800

    hty

--- oncecloud3-web/src/test/java/com/oncecloud/manager/LogManagerTest.java
+++ oncecloud3-web/src/test/java/com/oncecloud/manager/LogManagerTest.java
@@ -1,5 +1,6 @@
 package com.oncecloud.manager;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,5 +10,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ContextConfiguration(locations = "classpath*:./com/oncecloud/config/application-context.xml")
 @WebAppConfiguration
 public class LogManagerTest {
-
+	@Test
+	public void logTest() {
+//		List<OCLog> list = this.getLogDAO().getLogList(1, 0, 1, 10);
+//		for (OCLog ocLog : list) {
+//			System.out.println(ocLog.getLogInfo());
+//		}
+	}
 }
